3c128866efe2bac4ef418f5fe9248a23fdba7f23
luca@igor <luca@igor>
2017-07-30 16:06:27 +0200

    first

---
+++ src/Model/Geometry/LineString.php
@@ -0,0 +1,26 @@
+<?php
+namespace laxertu\GeoJSON\Model\Geometry;
+
+use laxertu\GeoJSON\helper\CoordinatesValidator;
+use laxertu\DataTree\DataTreeElement;
+
+class LineString extends AbstractGeometry
+{
+    protected $type = 'LineString';
+    protected $isAList = true;
+
+
+    public function setCoordinates(array $coordinatesList)
+    {
+
+        if (count($coordinatesList) < 2) {
+            throw new \InvalidArgumentException('LineString coordinates have to be at leat 2');
+        }
+
+        foreach ($coordinatesList as $coordinates) {
+            CoordinatesValidator::validateCoordinates($coordinates);
+            $this->setChild(new DataTreeElement('coordinates', $coordinatesList), 1);
+        }
+    }
+
+}
---
+++ tests/Geometry/LineStringTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace laxertu\GeoJSON\tests\Geometry;
+
+use laxertu\GeoJSON\Model\Geometry\LineString;
+
+class MultiPointTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  LineString */
+    private $sut;
+
+    protected function setUp()
+    {
+        $this->sut = new LineString();
+    }
+
+    public function testKo()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->sut->setCoordinates([[1,2,3]]);
+    }
+
+    public function testSetCoordinates()
+    {
+        $this->sut->setCoordinates([[1,2,3], [4,5,6]]);
+        $this->assertSame([[1,2,3], [4,5,6]], $this->sut->getChildren()[1]->getValue());
+    }
+
+}
