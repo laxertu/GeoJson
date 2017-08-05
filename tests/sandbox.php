e56970386b5780798e2681ce1eec5e3e717bdff1
luca@igor <luca@igor>
2017-08-05 20:26:35 +0200

    first

--- tests/sandbox.php
+++ tests/sandbox.php
@@ -1,9 +1,6 @@
 <?php
 namespace laxertu\GeoJSON\tests;
 
-use laxertu\DataTree\Processor\JsonFormatter;
-use laxertu\GeoJSON\Model\Geometry\LineString;
-
 error_reporting(E_ALL);
 
 require_once 'boot.php';
@@ -11,14 +8,3 @@ require_once 'print_utils.php';
 
 #------------------------------------------------------
 
-$mp = new LineString();
-$mp->setName('MyLineString');
-$mp->setCoordinates([[1,2,3], [4,5,6]]);
-
-$f = new JsonFormatter();
-
-$json = $f->buildContent($mp);
-
-oStr($json);
-
-oJson($json);
